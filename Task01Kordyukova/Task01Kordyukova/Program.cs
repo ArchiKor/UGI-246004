1c4a130c26e6d53846ff2b96ea0eccd8f6a51da6
ArchiKor <arina.kordyukova@gmail.com>
2025-09-24 19:55:29 +0500

    Задание 1

---
+++ Task01Kordyukova/Task01Kordyukova/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01Kordyukova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Франтишек Грубин");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Ещё не осень...");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ещё не осень! Если я");
+            Console.WriteLine("Терплю, как осень терпит лужи,");
+            Console.WriteLine("Печаль былого бытия,");
+            Console.WriteLine("Я знаю: завтра будет лучше.");
+
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
