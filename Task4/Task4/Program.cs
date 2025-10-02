1e10d20a668ced60d56ca2629e15c742bca9185c
ArchiKor <arina.kordyukova@gmail.com>
2025-10-02 16:39:25 +0500

    Задание 4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            var x = double.Parse(Console.ReadLine());
+
+            var y = F(x);
+
+            Console.WriteLine("y = " + y);
+
+        }
+
+
+        static double F(double x) => (Math.Abs(x + 1) - Math.Abs(x - 1)) / Math.Abs(x);
+        
+    }
+}
