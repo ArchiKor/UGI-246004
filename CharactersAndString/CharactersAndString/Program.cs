6cb93528addb92a39fa974a3e27af0a47851d2d2
ArchiKor <arina.kordyukova@gmail.com>
2025-10-08 20:26:22 +0500

    Задание 5

---
+++ CharactersAndString/CharactersAndString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharactersAndString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'z';
+
+            letter = (char)0x42F;
+
+            Console.WriteLine(letter);
+            Console.WriteLine(char.ToLower(letter));
+        }
+    }
+}
