a0890e0980e9e986f158cec311cb68470214f106
ArchiKor <arina.kordyukova@gmail.com>
2025-10-28 21:33:01 +0500

    Задание 8

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var rstr = Console.ReadLine();
+            var flag = false;
+            for (var charIndex = 0; charIndex < rstr.Length; charIndex++)
+            {
+                if (flag || rstr[charIndex] != '\\')
+                    Console.Write(rstr[charIndex]);
+                flag = rstr[charIndex] == '\\';
+            }
+        }
+    }
+}
---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число k");
+            var k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите целое число n");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите целое число m");
+            var m = int.Parse(Console.ReadLine());
+            if (IfLogicalExpressionTrue(k, n, m))
+                Console.WriteLine("k, n, m - положительные и хотя бы одно четное");
+            else
+                Console.WriteLine("Либо одно из чисел не положительное," + "либо нет четного числа");
+        }
+
+        static bool IfLogicalExpressionTrue(int k, int n, int m) =>
+            (k > 0 && m > 0 && n > 0) && (k % 2 == 0 || m % 2 == 0 || n % 2 == 0);
+    }
+}
