0e35072af4770f05c3bb9be476a46bfba9a573de
ArchiKor <arina.kordyukova@gmail.com>
2025-12-02 20:27:10 +0500

    Задание 10

---
+++ Task10.02/Task10.02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел n:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите 1-е число");
+            double past = double.Parse(Console.ReadLine());
+            double maxZnach = 0;
+
+            for (int k = 2; k <= n; k++)
+            {
+                Console.WriteLine($"Введите {k}-е число");
+                double current = double.Parse(Console.ReadLine());
+
+                double znach = Math.Abs(current - past);
+                if (znach > maxZnach)
+                {
+                    maxZnach = znach;
+                }
+
+                past = current;
+            }
+
+            Console.WriteLine($"Максимальное значение |ak - a(k-1)| = {maxZnach:F4}");
+        }
+    }
+}
---
+++ Task10.04/Task10.04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите последовательность целых чисел, оканчивающуюся нулем");
+
+            int sum = 0;
+            int number;
+
+            do
+            {
+                number = int.Parse(Console.ReadLine());
+
+                if (number > a)
+                {
+                    sum += number;
+                }
+            }
+            while (number != 0);
+
+            Console.WriteLine($"Сумма всех чисел последовательности, больших {a}: {sum}");
+        }
+    }
+}
