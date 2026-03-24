5471789801fa140c23557f41d33e023d10aa87c3
ArchiKor <arina.kordyukova@gmail.com>
2026-03-24 19:57:17 +0500

    Shapes

---
+++ Shapes/Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Shapes
+{
+    public class Circle
+    {
+        public point Centre;
+        public int Radius;
+
+        public Color FillColor;
+
+        public Color OutlineColor;
+    
+        public Circle(point centre, int radius, Color fillColor, Color outlineColor)
+        {
+            Centre = centre;
+            Radius = radius;
+            FillColor = fillColor;
+            OutlineColor = outlineColor;
+        }
+
+        public override string ToString() => $"круг с центром в точке {Centre} радиуса {Radius}";
+       
+    }
+
+}
---
+++ Shapes/ShapesApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shapes;
+using System.Drawing;
+namespace ShapesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Shapes.point(2, 5, Color.Blue);
+            Console.WriteLine(p);
+
+            var circle = new Circle(p, 3, Color.LightGreen, Color.Green);
+            Console.WriteLine(circle);
+        }
+    }
+}
